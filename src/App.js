d9963461f7c679d21d9d1cafe3a22e7f1cb5b2b3
priyankasinght <35567382+priyankasinght@users.noreply.github.com>
2022-10-31 18:32:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi delectus quasi iusto.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt nemo deleniti cupiditate libero. Quae sit corrupti quam deleniti ratione temporibus similique ad hic, ipsa laborum distinctio ullam explicabo unde, enim doloremque voluptas voluptatum quis quibusdam blanditiis inventore velit officia. Vitae totam modi atque similique, veritatis perferendis numquam, exercitationem voluptatum expedita consequatur itaque. Impedit vel, ullam aperiam et earum distinctio non. Consectetur consequatur, libero recusandae doloribus iusto laborum quia natus reiciendis veniam excepturi quidem fugit dolores nam, culpa ab. Nisi quisquam unde rerum eaque facere voluptas? Reiciendis aspernatur quo aperiam nulla ab soluta ipsam nihil cum, vero aliquid repellat dolor iusto.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aperiam officia facilis atque. Sequi eius sunt asperiores iure, rem maxime animi doloremque porro accusantium cum, repellat eaque totam soluta beatae voluptatum ut in quae odio ullam rerum. Consequatur, nesciunt fuga!</p>
+    </div>
+  );
+}
+
+export default App;
